dfd676d9246ea56dd8f26552ebc4c3d1affb456c
Recuenco, Cyril B <156570207+Vyrik554@users.noreply.github.com>
2024-04-09 20:29:45 +0800

    Add files via upload

---
+++ LAB2/LR2_2.py
@@ -0,0 +1,22 @@
+def navigate_file_lines(filename):
+    # Open the file and read the lines into a list
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+
+    while True:
+        # Print the number of lines in the file
+        print(f"The file has {len(lines)} lines.")
+
+        # Prompt the user for a line number
+        line_number = int(input("Enter a line number (or 0 to quit): "))
+
+        # If the input is 0, quit the program
+        if line_number == 0:
+            break
+
+        # Otherwise, print the line associated with that number
+        else:
+            print(lines[line_number - 1])
+
+# Call the function with a filename
+navigate_file_lines("numbers.txt")
