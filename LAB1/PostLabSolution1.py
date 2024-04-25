4e90fa65241194d53706c89dc2789bbb1e0a3a9c
Altaire Española <160741584+altairefaith@users.noreply.github.com>
2024-04-25 12:08:25 +0800

    add: file PostLabSolution1.py

---
+++ LAB1/PostLabSolution1.py
@@ -0,0 +1,57 @@
+"""
+Statisticians would like to have a set of functions
+to compute the median and mode of a list of numbers.
+The median is the number that would appear at the
+midpoint of a list if it were sorted. The mode is
+the number that appears most frequently in the list.
+Define these functions in a module named stats.py.
+Also include a function named mean, which computes
+the average of a set of numbers. Each function expects
+a list of numbers as an argument and returns a single number.
+"""
+
+print("Input numbers, and type '-999' if done")
+
+num = 0
+userlist = []
+
+while num != (-999):
+    num = int(input("Enter value: "))
+    userlist.append(num)
+
+userlist.remove(-999)
+
+print("List:" , userlist) 
+print("List Length:" , len(userlist))
+
+def compMedian(lst):
+    lst.sort()
+    length = len(lst)
+    if length % 2 == 0:
+        x = length//2
+        median = (lst[x-1]+lst[x])/2
+    else:
+        x = (length-1)//2
+        median = lst[x]
+    return median
+
+median = compMedian(userlist)
+print("The median is" , median)
+
+
+def compMode(lst):
+    freq = max(list(map(lst.count, lst))) #finds what number shows up more often
+    mode = list(set(filter(
+        lambda x: lst.count(x) == freq, lst))) #func to see other modes; numbers with equal freq
+    return mode # returns a list
+
+mode = compMode(userlist)
+print("The mode is" , mode)
+
+
+def compMean(lst):
+    mean = sum(lst) / len(lst)
+    return mean
+
+mean = compMean(userlist)
+print("The mean is" , mean)
